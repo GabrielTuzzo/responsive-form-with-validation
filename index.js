d09e4a3f2628c79351f8a1ce8c9ec68f0a15f3d3
Gabriel Tuzzo <76889705+GabrielTuzzo@users.noreply.github.com>
2022-08-25 12:54:08 +0100

    new commit

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ username.addEventListener('keyup', () => {
     const name = username.value
 
     if (name.value === '') {
-        nameError.innerHTML = 'Name is required'
+        nameError.innerHTML = '<i class="fas fa-solid fa-circle-xmark"></i>'
+        nameError.innerHTML += '<i class="fas fa-solid fa-circle-xmark"></i>'
         return false
     }
     if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
         nameError.innerHTML = 'Write full name'
+        nameError.innerHTML += '<i class="fas fa-solid fa-circle-xmark"></i>'
         return false
     }
     nameError.innerHTML = '<i class="fa fa-solid fa-check"></i>'
@@ -29,10 +31,12 @@ emailInput.addEventListener('keyup', () => {
 
     if (email.value === '') {
         emailError.innerHTML = 'Email is required'
+        emailError.innerHTML += '<i class="fas fa-solid fa-circle-xmark"></i>'
         return false
     }
     if (!email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
         emailError.innerHTML = 'Write a valid email'
+        emailError.innerHTML += '<i class="fas fa-solid fa-circle-xmark"></i>'
         return false
     }
 
@@ -44,10 +48,12 @@ phoneInput.addEventListener('keyup', () => {
 
     if (phone.value === '') {
         phoneNumberError.innerHTML = 'Phone number is required'
+        phoneNumberError.innerHTML += '<i class="fas fa-solid fa-circle-xmark"></i>'
         return false
     }
     if (phone.lenght <= 12) {
         phoneNumberError.innerHTML = 'write a valid phone number'
+        phoneNumberError.innerHTML += '<i class="fas fa-solid fa-circle-xmark"></i>'
         return false
     }
     phoneNumberError.innerHTML = '<i class="fa fa-solid fa-check"></i>'
